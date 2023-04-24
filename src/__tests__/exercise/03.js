f977edb565da645f727184ba53463f03452eaa20
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-04-24 11:08:00 +0330

    ex0301: use userEvent instead of fireEvent to make the tests more implementation free.

--- src/__tests__/exercise/03.js
+++ src/__tests__/exercise/03.js
@@ -2,18 +2,19 @@
 // http://localhost:3000/counter
 
 import * as React from 'react'
-import {render, fireEvent, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {render, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
 
-test('counter increments and decrements when the buttons are clicked', () => {
+test('counter increments and decrements when the buttons are clicked', async () => {
   render(<Counter />)
   const increment = screen.getByRole('button', {name: /increment/i})
   const decrement = screen.getByRole('button', {name: 'Decrement'})
   const message = screen.getByText('Current count: 0')
 
   expect(message).toHaveTextContent('Current count: 0')
-  fireEvent.click(increment)
+  await userEvent.click(increment)
   expect(message).toHaveTextContent('Current count: 1')
-  fireEvent.click(decrement)
+  await userEvent.click(decrement)
   expect(message).toHaveTextContent('Current count: 0')
 })
