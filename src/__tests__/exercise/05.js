16d6ffe617e4602df3ac3011f79551169c778c51
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-04-29 11:09:15 +0330

    ex0504: use one-off server handlersto show an error when the server sends a 500 status code.

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -8,6 +8,7 @@ import {build, fake} from '@jackfranklin/test-data-bot'
 import {setupServer} from 'msw/node'
 import {handlers} from 'test/server-handlers'
 import Login from '../../components/login-submission'
+import {rest} from 'msw'
 
 const buildLoginForm = build({
   fields: {
@@ -19,6 +20,7 @@ const buildLoginForm = build({
 const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
 test(`logging in displays the user's username`, async () => {
@@ -62,4 +64,23 @@ test(`should display an error if the password is not provided`, async () => {
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
   )
+})
+
+test(`should show an error when the server misbehaves and sends a status code of 500`, async () => {
+  const testErrorMessage = 'Oh no, something bad happened'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (_req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+
+  render(<Login />)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
