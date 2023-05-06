e26a4490817dfaa57e616802bb3dfe387ca7e9fa
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-05-06 12:17:44 +0330

    ex0602: test the unhappy path.

--- src/__tests__/exercise/06.js
+++ src/__tests__/exercise/06.js
@@ -1,12 +1,24 @@
 // mocking Browser APIs and modules
 // http://localhost:3000/location
 
-import * as React from 'react'
+import React from 'react'
 import {render, screen, act} from '@testing-library/react'
-import {useCurrentPosition} from 'react-use-geolocation'
 import Location from '../../examples/location'
 
-jest.mock('react-use-geolocation')
+beforeAll(() => {
+  window.navigator.geolocation = {
+    getCurrentPosition: jest.fn(),
+  }
+})
+
+function deferred() {
+  let resolve, reject
+  const promise = new Promise((res, rej) => {
+    resolve = res
+    reject = rej
+  })
+  return {promise, resolve, reject}
+}
 
 test('displays the users current location', async () => {
   const fakePosition = {
@@ -16,20 +28,20 @@ test('displays the users current location', async () => {
     },
   }
 
-  let setReturnValue
-  function useMockCurrentPosition() {
-    const state = React.useState([])
-    setReturnValue = state[1]
-    return state[0]
-  }
-  useCurrentPosition.mockImplementation(useMockCurrentPosition)
+  const {promise, resolve} = deferred()
+  window.navigator.geolocation.getCurrentPosition.mockImplementation(
+    callback => {
+      promise.then(() => callback(fakePosition))
+    },
+  )
 
   render(<Location />)
 
   expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
 
-  act(() => {
-    setReturnValue([fakePosition])
+  await act(async () => {
+    resolve()
+    await promise
   })
   // screen.debug()
 
