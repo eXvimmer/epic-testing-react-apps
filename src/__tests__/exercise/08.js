0dabf30c39ecf98b1af22db7924a2ed5f4524641
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-05-07 13:48:40 +0330

    ex0801: use a fake component for testing hooks.

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -2,28 +2,22 @@
 // http://localhost:3000/counter-hook
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-function TestComponent() {
-  const {count, decrement, increment} = useCounter()
-  return (
-    <>
-      <h1>Current count: {count}</h1>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-    </>
-  )
-}
-
 test('exposes the count and increment/decrement functions', async () => {
+  let result
+  function TestComponent() {
+    result = useCounter()
+    return null
+  }
   render(<TestComponent />)
-  const countEl = screen.getByText(/current count/i)
-  expect(countEl).toHaveTextContent('Current count: 0')
 
-  await userEvent.click(screen.getByRole('button', {name: '+'}))
-  expect(countEl).toHaveTextContent('Current count: 1')
-  await userEvent.click(screen.getByRole('button', {name: '-'}))
-  expect(countEl).toHaveTextContent('Current count: 0')
+  expect(result.count).toBe(0)
+
+  act(result.increment)
+  expect(result.count).toBe(1)
+
+  act(result.decrement)
+  expect(result.count).toBe(0)
 })
