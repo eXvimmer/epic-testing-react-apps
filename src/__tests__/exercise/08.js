edf0ec39f2d3990b83615de2ae6c15483f691b42
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-05-07 16:09:17 +0330

    ex0803: replace setup function with renderHook.

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,57 +1,39 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, act} from '@testing-library/react'
+import {renderHook, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
 test('exposes the count and increment/decrement functions', async () => {
-  let result
-  function TestComponent() {
-    result = useCounter()
-    return null
-  }
-  render(<TestComponent />)
+  const {result} = renderHook(useCounter)
+  expect(result.current.count).toBe(0)
 
-  expect(result.count).toBe(0)
+  act(result.current.increment)
+  expect(result.current.count).toBe(1)
 
-  act(result.increment)
-  expect(result.count).toBe(1)
-
-  act(result.decrement)
-  expect(result.count).toBe(0)
+  act(result.current.decrement)
+  expect(result.current.count).toBe(0)
 })
 
-function setup(...args) {
-  const result = {}
-  function TestComponent() {
-    Object.assign(result, useCounter(...args))
-    return null
-  }
-
-  render(<TestComponent />)
-  return result
-}
-
 test('allows customization of initial count', () => {
-  const counterData = setup({initialCount: 2})
+  const {result} = renderHook(useCounter, {initialProps: {initialCount: 2}})
 
-  expect(counterData.count).toBe(2)
+  expect(result.current.count).toBe(2)
 
-  act(() => counterData.increment())
-  expect(counterData.count).toBe(3)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(3)
 
-  act(() => counterData.decrement())
-  expect(counterData.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(2)
 })
 
 test('allows customization of the step', () => {
-  const counterData = setup({step: 2})
-  expect(counterData.count).toBe(0)
+  const {result, rerender} = renderHook(useCounter, {initialProps: {step: 2}})
+  expect(result.current.count).toBe(0)
 
-  act(counterData.increment)
-  expect(counterData.count).toBe(2)
-
-  act(counterData.decrement)
-  expect(counterData.count).toBe(0)
+  act(result.current.increment)
+  expect(result.current.count).toBe(2)
+  rerender({step: 2})
+  act(result.current.decrement)
+  expect(result.current.count).toBe(0)
 })
